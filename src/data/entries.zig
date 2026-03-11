d9ec6a4fec2430198c5e784b7366810c61e6c6c4
Cristiano <cs.almeida@protonmail.ch>
2026-03-11 19:18:44 -0300

    Adds datasets, Person and random picker. Initial datasets, Person has random values.

---
+++ src/data/entries.zig
@@ -0,0 +1,12 @@
+/// This file exposes all data sets through a single module.
+/// This access so that it can be used as follows:
+///
+/// const data = @import("data/entries.zig");
+/// const emails = data.emails.items;
+///
+
+pub const names              = @import("names.zig");
+pub const emails             = @import("emails.zig");
+pub const phones             = @import("phones.zig");
+pub const domains            = @import("domains.zig");
+pub const organizations      = @import("organizations.zig");
